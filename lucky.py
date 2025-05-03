1bf7f6459cdf4c0ad1dbe383b74ca60c27042eb6
Zeur5-6 <phef0506@gmail.com>
2025-05-04 02:38:13 +0900

    day2: add input and error handling

--- lucky.py
+++ lucky.py
@@ -1,9 +1,19 @@
 import random
 from datetime import datetime
 import math
+import sys
 
 today = datetime.now().date()
-lucky_num = random.randint(1, 99)
+while True:
+    raw = input("1~99の任意の数字を入力▶")
+    try:
+        lucky_num = int(raw)
+        if not 1 <= lucky_num <= 99:
+            raise ValueError
+        break
+    except ValueError:
+        print("ちゃんとやれ")
+        sys.exit(1)
 
 # ついでに math を使って“余興”を足す
 digit_sum = sum(int(d) for d in str(lucky_num))
